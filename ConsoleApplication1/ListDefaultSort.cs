5b7a9f76e3fcad58c3a4d8246819c943d56a1fd7
unknown <bowen.zhang@BOWEN-PC.suryani.cn>
2014-09-16 16:46:18 +0800

    in s.c

---
+++ ConsoleApplication1/ListDefaultSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ListDefaultSort
+    {
+        /*
+If the partition size is fewer than 16 elements, it uses an insertion sort algorithm.
+If the number of partitions exceeds 2 * LogN, where N is the range of the input array, it uses a Heapsort algorithm.
+Otherwise, it uses a Quicksort algorithm.
+         */
+
+        public static void Test16()
+        {
+            List<ListSortItem> list = new List<ListSortItem>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(new ListSortItem() { Id = i, Value = i % 3 });
+            }
+
+            list.Sort();
+            Console.WriteLine("==============");
+            list.ForEach(Console.WriteLine);
+        }
+
+    }
+
+    class ListSortItem : IComparable
+    {
+        public int Id;
+        public int Value;
+
+        public int CompareTo(object obj)
+        {
+            var item = obj as ListSortItem;
+            if (item == null) return 1;
+            return this.Value.CompareTo(item.Value);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Id={0}, Value={1}", Id, Value);
+        }
+    }
+}
---
+++ ConsoleApplication1/ReferenceFinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ConsoleApplication1
+{
+    class DllConstant
+    {
+        public const string DllBaseDir = @"E:\Code\WWW\DEV\Diapers\WebSite\Bin\";
+
+        public static readonly string ModelDll = DllBaseDir + @"Model.dll";
+
+        public static readonly string DataDll = DllBaseDir + @"DataAccess.dll";
+
+        public static readonly string BusinessDll = DllBaseDir + @"Business.dll";
+
+        public static readonly string QuidsiWebSiteDll = DllBaseDir + @"QuidsiWebSite.dll";
+
+        public static readonly string QuidsiServiceDll = DllBaseDir + @"QuidsiService.dll";
+
+        public static readonly string WebSiteBaseDll = DllBaseDir + @"WebSiteBase.dll";
+    }
+
+    class ReferenceFinder
+    {
+        /*
+         * 1. 获取到dll里的所有类型
+         * 2. 获取到类型里的所有property
+         * 3. 获取到类型里的所有方法
+         * 4. 获取方法的返回值
+         * 5. 获取方法的参数
+         * 6. 获取方法的方法体
+        */
+
+        public static List<Type> GetAllTypes(string dll)
+        {
+            var ass = Assembly.UnsafeLoadFrom(dll);
+            return ass.GetTypes().ToList();
+        }
+    }
+}
