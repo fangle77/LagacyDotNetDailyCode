e50be86e0e70a3b03c8c9e75bc7a337b7ff2bc5f
howbigbobo <whatagoodmail@gmail.com>
2014-09-27 21:35:22 +0800

    11

---
+++ Pineapple/Pineapple.Service/CategoryService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+using Pineapple.Business;
+using Pineapple.Model;
+using Pineapple.View;
+
+namespace Pineapple.Service
+{
+    public class CategoryService
+    {
+        [Dependency]
+        public CategoryManager CategoryManager { protected get; set; }
+
+        [Dependency]
+        public CatalogManager CatalogManager { protected get; set; }
+
+        [Dependency]
+        public NavigationManager NavigationManager { protected get; set; }
+
+        public virtual Category SaveCategory(Category category)
+        {
+            return CategoryManager.SaveCategory(category);
+        }
+
+        public virtual bool DeleteCategory(int categoryId)
+        {
+            return CategoryManager.DeleteCategory(categoryId);
+        }
+
+        public virtual CategoryItem SaveCategoryItem(CategoryItem categoryItem)
+        {
+            return CategoryManager.SaveCategoryItem(categoryItem);
+        }
+
+        public virtual CategoryItem GetCategoryItemById(int categoryItemId)
+        {
+            return CategoryManager.GetCategoryItemById(categoryItemId);
+        }
+
+        public virtual bool DeleteCategoryItems(IEnumerable<int> categoryItemIds)
+        {
+            return CategoryManager.DeleteCategoryItems(categoryItemIds);
+        }
+
+        public CategoryView GetCategoryViewByCategryid(int categoryId)
+        {
+            CategoryView view = new CategoryView();
+            view.Category = CategoryManager.GetCategoryById(categoryId);
+            if (view.Category == null) return null;
+
+            view.Catalogs = CatalogManager.LoadCatalogByCategoryId(categoryId);
+            view.Navigations = NavigationManager.LoadNavigationsByCategoryId(categoryId);
+            view.CategoryItems = CategoryManager.LoadCategoryItemsByCategoryId(categoryId);
+
+            return view;
+        }
+    }
+}
---
+++ Pineapple/Pineapple.View/CategoryView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using Pineapple.Model;
+
+namespace Pineapple.View
+{
+    public class CategoryView
+    {
+        public Category Category { get; set; }
+
+        public List<CategoryItem> CategoryItems { get; set; }
+
+        public List<Catalog> Catalogs { get; set; }
+
+        public List<Navigation> Navigations { get; set; }
+
+        public List<Template> Templates { get; set; }
+    }
+}
