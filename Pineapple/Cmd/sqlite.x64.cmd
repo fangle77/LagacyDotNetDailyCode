6d6cbdafc4638e27e2cb104d39818186d1d8ddf0
howbigbobo <whatagoodmail@gmail.com>
2015-04-15 16:26:19 +0800

    sqlite. x64 x86 ....

--- Pineapple/Cmd/sqlite.x64.cmd
+++ Pineapple/Cmd/sqlite.x64.cmd
@@ -1,2 +1,6 @@
-copy "..\Libraly\Sqlite.Data.x64\*.dll " "..\Pineapple.Data.Sqlite\SqliteDll"
+xcopy "..\Libraly\Sqlite.Data.x64\*" "..\Pineapple.Data.Sqlite\SqliteDll" /s /i /y
+xcopy "..\Libraly\Sqlite.Data.x64\*" "..\Pineapple.WebSite\bin" /s /i /y
+xcopy "..\Libraly\Sqlite.Data.x64\*" "..\UnitTest\Pineapple.UnitTest\bin\Debug" /s /i /y
+xcopy "..\Libraly\Sqlite.Data.x64\*" "..\UnitTest\Pineapple.UnitTest\bin\Release" /s /i /y
+
 pause
--- Pineapple/Cmd/sqlite.x86.cmd
+++ Pineapple/Cmd/sqlite.x86.cmd
@@ -1,2 +1,6 @@
-copy "..\Libraly\Sqlite.Data.x86\*.dll " "..\Pineapple.Data.Sqlite\SqliteDll"
+xcopy "..\Libraly\Sqlite.Data.x86\*" "..\Pineapple.Data.Sqlite\SqliteDll" /s /i /y
+xcopy "..\Libraly\Sqlite.Data.x86\*" "..\Pineapple.WebSite\bin" /s /i /y
+xcopy "..\Libraly\Sqlite.Data.x86\*" "..\UnitTest\Pineapple.UnitTest\bin\Debug" /s /i /y
+xcopy "..\Libraly\Sqlite.Data.x86\*" "..\UnitTest\Pineapple.UnitTest\bin\Release" /s /i /y
+
 pause
