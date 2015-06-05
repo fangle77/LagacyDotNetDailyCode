1a1dee52c002c6e0997996c342536c9c28b25c74
howbigbobo <whatagoodmail@gmail.com>
2015-06-05 18:07:37 +0800

    111

--- Pineapple/Pineapple.WebSite/Controllers/BaseController.cs
+++ Pineapple/Pineapple.WebSite/Controllers/BaseController.cs
@@ -9,27 +9,36 @@ namespace Pineapple.WebSite.Controllers
 	/// </summary>
 	public abstract class BaseController : Controller
 	{
-		[Dependency("DefaultViewDir")]
+		[Dependency("DefaultViewTemplate")]
 		public string DefaultViewDir
 		{
 			protected get; set;
 		}
 		
-		protected string ViewDir
+		public virtual string ViewTemplate()
 		{
-			get
-			{
-				string cookieView =	base.ControllerContext.RequestContext.HttpContext.Request.Cookies["_v_"];
+				string cookieView =	base.ControllerContext.RequestContext.HttpContext.Request.Cookies["_v_t_"];
 				if(String.IsNullOrEmpty(cookieView)){
 					cookieView = DefaultViewDir;
-					HttpCookie cookie = new HttpCookie("_v_",cookieView);
-					cookie.Expires = DateTime.Now.AddYears(1);
+					HttpCookie cookie = new HttpCookie("_v_t_",cookieView);
+					cookie.Expires = DateTime.Now.AddDays(30);
 					base.ControllerContext.RequestContext.HttpContext.Response.SetCookie(cookie);
 				}
 				return cookieView;
-			}
 		}
 		
+		protected string Master { get { return String.Format("~/Views/{0}/_Layout.cshtml",ViewTemplate()); } }
 		
+		protected string AddManageBase(string viewName)
+        {
+			string viewTemplate = ViewTemplate();
+            return string.Format("~/Views/{0}/{1}.cshtml", viewTemplate, viewName);
+        }
+		
+		protected override ViewResult View(string viewName, string masterName, object model)
+		{
+			viewName = AddManageBase(viewName);
+			return base.View(viewName, Master, model);
+		}
 	}
 }
